6752ac74bc9a1f9148526ffd9b6b766fb18e25c1
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-17 15:46:45 +0530

    get started DONE

--- lib/screens/get_started.dart
+++ lib/screens/get_started.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
@@ -9,7 +10,63 @@ class GetStarted extends StatelessWidget {
       body: Container(
         width: double.infinity,
         decoration: BoxDecoration(
-            image: DecorationImage(image: AssetImage('assets/images/bg.png'))),
+            image: DecorationImage(
+                image: AssetImage('assets/images/bg.png'), fit: BoxFit.cover)),
+        child: Container(
+          decoration: BoxDecoration(
+              gradient: LinearGradient(begin: Alignment.bottomCenter, colors: [
+            Colors.black.withOpacity(.7),
+            Colors.black.withOpacity(.4)
+          ])),
+          child: Padding(
+            padding: EdgeInsets.only(left: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: 230,
+                ),
+                Text('"Everyone\nCan be Great\nbecause\neveryone can Serve"',
+                    style: GoogleFonts.inter(
+                        fontSize: 30,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 25,
+                ),
+                Text(
+                  "- Martin Luther King -",
+                  style: GoogleFonts.inter(color: Colors.white, fontSize: 18),
+                ),
+                SizedBox(
+                  height: 200,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 20),
+                  child: Container(
+                    height: 50,
+                    decoration: BoxDecoration(
+                        color: Color.fromARGB(255, 105, 0, 98),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: Center(
+                      child: InkWell(
+                        onTap: () {},
+                        child: Text(
+                          "Get Started",
+                          style: GoogleFonts.inter(
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
