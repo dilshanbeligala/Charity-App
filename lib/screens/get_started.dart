46530d7bcd061be5652f814a6ee3917a4ac3b336
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-17 14:54:13 +0530

    first commit

---
+++ lib/screens/get_started.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GetStarted extends StatelessWidget {
+  const GetStarted({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/images/bg.png'))),
+      ),
+    );
+  }
+}
