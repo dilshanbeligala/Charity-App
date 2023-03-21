15ef60f6c2ce9e6d142d7b96911e597d40b9208e
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-21 10:34:32 +0530

    side bar customized

--- lib/screens/charity_category/education.dart
+++ lib/screens/charity_category/education.dart
@@ -5,8 +5,33 @@ class Education extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text("education")),
-    );
+    return Scaffold(
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 105, 0, 98),
+          title: Padding(
+            padding: const EdgeInsets.only(left: 70),
+            child: Text("අත්වැල"),
+          ),
+        ),
+        body: Container(
+            margin: EdgeInsets.only(top: 5, bottom: 20),
+            width: MediaQuery.of(context).size.width,
+            height: 55,
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  hintText: "Search here....",
+                  hintStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    size: 25,
+                  )),
+            )));
   }
 }
