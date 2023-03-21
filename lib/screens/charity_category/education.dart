42767a2479b600d7949ec2879049c7a64a518ff4
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-21 10:39:54 +0530

    education page

--- lib/screens/charity_category/education.dart
+++ lib/screens/charity_category/education.dart
@@ -13,25 +13,30 @@ class Education extends StatelessWidget {
             child: Text("අත්වැල"),
           ),
         ),
-        body: Container(
-            margin: EdgeInsets.only(top: 5, bottom: 20),
-            width: MediaQuery.of(context).size.width,
-            height: 55,
-            alignment: Alignment.center,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20)),
-                  hintText: "Search here....",
-                  hintStyle: TextStyle(color: Colors.black.withOpacity(0.5)),
-                  prefixIcon: Icon(
-                    Icons.search,
-                    size: 25,
-                  )),
-            )));
+        body: Padding(
+          padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+          child: Container(
+              margin: EdgeInsets.only(top: 5, bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextFormField(
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: "Search here....",
+                    hintStyle: TextStyle(
+                      color: Color.fromARGB(255, 105, 0, 98),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 25,
+                      color: Color.fromARGB(255, 105, 0, 98),
+                    )),
+              )),
+        ));
   }
 }
