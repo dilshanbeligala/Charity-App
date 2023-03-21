81de0e9f679ef76a24cfb413bd393d08dc35b3a0
dilshanbeligala <beligaladilshan@gmail.com>
2023-03-21 17:41:48 +0530

    search district DONE

---
+++ lib/model/charity_model.dart
@@ -0,0 +1,7 @@
+class Charity_Model {
+  String? district;
+  
+  
+
+  Charity_Model(this.district);
+}
--- lib/screens/charity_category/oters.dart
+++ lib/screens/charity_category/oters.dart
@@ -1,10 +1,52 @@
-import 'package:charity_app/screens/forms/othersF.dart';
+import 'package:charity_app/model/charity_model.dart';
+import 'package:charity_app/screens/forms/educatinF.dart';
 import 'package:charity_app/screens/notification.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class Others extends StatelessWidget {
+class Others extends StatefulWidget {
   const Others({super.key});
+
+  @override
+  State<Others> createState() => _OthersState();
+}
+
+class _OthersState extends State<Others> {
+  static List<Charity_Model> district_list = [
+    Charity_Model("Ampara"),
+    Charity_Model("Anuradhapura"),
+    Charity_Model("Badulla"),
+    Charity_Model("Batticaloa"),
+    Charity_Model("Galle"),
+    Charity_Model("Gampaha"),
+    Charity_Model("Hambantota"),
+    Charity_Model("Jaffna"),
+    Charity_Model("Kaluthara"),
+    Charity_Model("Kandy"),
+    Charity_Model("Kegalle"),
+    Charity_Model("Kilinochchi"),
+    Charity_Model("Kurunegala"),
+    Charity_Model("Mannar"),
+    Charity_Model("Matale"),
+    Charity_Model("Matara"),
+    Charity_Model("Monaragala"),
+    Charity_Model("Mullativu"),
+    Charity_Model("Polonnaruwa"),
+    Charity_Model("Puttalam"),
+    Charity_Model("Ratnapura"),
+    Charity_Model("Trincomalee"),
+    Charity_Model("Vauniya"),
+  ];
+  List<Charity_Model> display_list = List.from(district_list);
+
+  void updateList(String value) {
+    setState(() {
+      display_list = district_list
+          .where((element) =>
+              element.district!.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,7 +81,7 @@ class Others extends StatelessWidget {
                   child: InkWell(
                     onTap: () {
                       Navigator.push(context,
-                          MaterialPageRoute(builder: (_) => Others_form()));
+                          MaterialPageRoute(builder: (_) => Edu_form()));
                     },
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -71,27 +113,47 @@ class Others extends StatelessWidget {
               height: 20,
             ),
             Container(
-                margin: EdgeInsets.only(top: 5, bottom: 20),
-                width: MediaQuery.of(context).size.width,
-                height: 55,
-                alignment: Alignment.center,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: UnderlineInputBorder(),
-                      hintText: "Search district here....",
-                      hintStyle: TextStyle(
-                        color: Color.fromARGB(255, 105, 0, 98),
-                      ),
-                      prefixIcon: Icon(
-                        Icons.search,
-                        size: 25,
-                        color: Color.fromARGB(255, 105, 0, 98),
+              margin: EdgeInsets.only(top: 5, bottom: 20),
+              width: MediaQuery.of(context).size.width,
+              height: 55,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: TextField(
+                onChanged: ((value) => updateList(value)),
+                decoration: InputDecoration(
+                    border: UnderlineInputBorder(),
+                    hintText: "Search district here....",
+                    hintStyle: TextStyle(
+                      color: Color.fromARGB(255, 105, 0, 98),
+                    ),
+                    prefixIcon: Icon(
+                      Icons.search,
+                      size: 25,
+                      color: Color.fromARGB(255, 105, 0, 98),
+                    )),
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: display_list.length,
+                  itemBuilder: (context, index) => ListTile(
+                        contentPadding: EdgeInsets.all(8.0),
+                        title: Text(
+                          display_list[index].district!,
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold),
+                        ),
+                        onTap: (() {
+                          // Navigator.push(
+                          //     context,
+                          //     MaterialPageRoute(
+                          //         builder: ((context) => ShedPage())));
+                        }),
                       )),
-                )),
+            )
           ],
         ),
       ),
